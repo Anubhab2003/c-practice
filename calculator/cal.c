6d77560cd92cf1d3c25c573067a84d048f57e244
Anubhab2003 <anubhabchowdhury01@gmail.com>
2022-07-27 23:05:20 +0530

    Added Sum Program

--- calculator/cal.c
+++ calculator/cal.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 int main(){
 	
-	int testInteger;
+	int num1,num2,sum;
 	printf("Enter A Number : ");
-	scanf("%d", &testInteger);
-	printf("Number = %d", testInteger);
+	scanf("%d", &num1);
+	printf("Enter ANother Number :");
+	scanf("%d", &num2);
+	sum = num1 + num2;
+	printf("Sum = %d", sum);
 	return 0;
 
 }
